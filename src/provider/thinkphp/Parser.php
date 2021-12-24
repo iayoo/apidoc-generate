ea8f87af80cc062830070dea81f572b2f5a8ea3b
Iayoo <vilnius94@163.com>
2021-12-24 16:06:48 +0800

    # 新增路由处理回调结果

--- src/provider/thinkphp/Parser.php
+++ src/provider/thinkphp/Parser.php
@@ -6,6 +6,7 @@ namespace Iayoo\ApidocGenerate\provider\thinkphp;
 
 use think\event\RouteLoaded;
 use think\Request;
+use think\Route;
 
 class Parser
 {
@@ -19,10 +20,10 @@ class Parser
         $router = app()->make(\think\Route::class);
 
         if (app()->runningInConsole()){
-            // 判断终端运行,thinkphp 终端模式下是不会加载路由的
+            // 判断终端运行,thinkphp 终端模式下是不会加载路由的,因此我们需要手动加载一次
             $this->loadRoutes();
         }
-        return $router->getRuleList();
+        return $router->getRuleName()->getRuleList();
     }
 
     /**
@@ -38,6 +39,30 @@ class Parser
             $files = glob($routePath . '*.php');
             foreach ($files as $file) {
                 include $file;
+            }
+        }
+    }
+    
+    public function routeHandle($callback){
+        $routes = $this->getRouteList();
+        $router = app()->make(Route::class);
+
+        $postmanItemList = [];
+        foreach ($routes as $route){
+            $ruleInfo = $router->getRuleName()->getRule($route['rule']);
+            $group = "default";
+            foreach ($ruleInfo as $itemInfo){
+                dump($itemInfo);
+                $group = $itemInfo->getParent()->getFullName();
+                if (empty($group)){
+                    $group = "default";
+                }
+            }
+            if (strstr($route['rule'],"<MISS>")!==false){
+                continue;
+            }
+            if ($callback instanceof \Closure){
+                $callback($route['rule'],$route['method'],[],$group,'');
             }
         }
     }

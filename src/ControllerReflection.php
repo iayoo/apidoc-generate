1d52a65b4012d18a31283b3c1187f024074c2272
Iayoo <vilnius94@163.com>
2021-12-24 14:04:14 +0800

    # 新增获取函数注释接口

--- src/ControllerReflection.php
+++ src/ControllerReflection.php
@@ -7,6 +7,12 @@ class ControllerReflection
 
     /** @var ReflectionClass  */
     protected $reflectClass;
+    /** @var DocParser */
+    protected $docParser;
+
+    protected $classCommentData = [];
+
+    protected $method;
 
     /**
      * @return string
@@ -29,12 +35,64 @@ class ControllerReflection
         if (!$this->reflectClass){
             $this->reflectClass = null;
         }
-        $this->reflectClass = new ReflectionClass ( $this->class );
-
+        $this->reflectClass = new \ReflectionClass ( $this->class );
+        $this->docParser = new DocParser();
+        $this->classCommentData = $this->docParser->parse( $this->reflectClass->getDocComment());
     }
 
-    public function getName(){
-        dump($this->reflectClass->getDocComment());
+    /**
+     * @return array
+     */
+    public function getClassCommentData(): array
+    {
+        return $this->classCommentData;
+    }
+
+    /**
+     * @param mixed $method
+     */
+    public function setMethod($method): void
+    {
+        $this->method = $method;
+    }
+
+    public function parserMethod(){
+        //获取类中的方法，设置获取public,protected类型方法
+        $methods = $this->reflectClass->getMethods(\ReflectionMethod::IS_PUBLIC + \ReflectionMethod::IS_PROTECTED + \ReflectionMethod::IS_PRIVATE);
+//遍历所有的方法
+        foreach ($methods as $method) {
+            if ($method->getName() !== $this->method){
+                continue;
+            }
+            $arguments = [];
+            $defaults = [];
+            $class_metadata = [];
+            //获取方法的注释
+            $doc = $method->getDocComment();
+            //解析注释
+            $info = $this->docParser->parse($doc);
+            $metadata = $class_metadata +  $info;
+            //获取方法的类型
+            $method_flag = $method->isProtected();//还可能是public,protected类型的
+            //获取方法的参数
+            $params = $method->getParameters();
+            $position=0;    //记录参数的次序
+            foreach ($params as $param){
+                $arguments[$param->getName()] = $position;
+                //参数是否设置了默认参数，如果设置了，则获取其默认值
+                $defaults[$position] = $param->isDefaultValueAvailable() ? $param->getDefaultValue() : NULL;
+                $position++;
+            }
+
+            $call = array(
+                'method_name'=>$method->getName(),
+                'arguments'=>$arguments,
+                'defaults'=>$defaults,
+                'metadata'=>$metadata,
+                'method_flag'=>$method_flag
+            );
+            return $call;
+        }
     }
 
 }
